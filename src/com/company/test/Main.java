a7240e83c0fd35aaa33aef32f5be1905059bd93e
Garnik888 <garnikvardanyan86@gmail.com>
2022-03-21 15:09:26 +0400

    Update homework day 23

--- src/com/company/test/Main.java
+++ src/com/company/test/Main.java
@@ -1,22 +1,11 @@
 package com.company.test;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List <Integer> list = new LinkedList<>();
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-
-        for (Integer integer : list);
-
-        System.out.println(list);
     }
 }
