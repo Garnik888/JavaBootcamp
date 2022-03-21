bd118b2968c47e15fa60b6acf90b3ee9e75b26ee
Garnik888 <garnikvardanyan86@gmail.com>
2022-03-22 00:21:18 +0400

    Add homework day 24

--- src/com/company/day24/Main.java
+++ src/com/company/day24/Main.java
@@ -8,55 +8,153 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyIntegerArrayList arrayList = new MyIntegerArrayList();
+        int array[] = {1, 2, 3, 4, 5, 1, 2, 3, 4};
 
-//        System.out.println("Size array list - " + arrayList.size());
-//        System.out.println("Is array list empty - " + arrayList.isEmpty());
+        System.out.println(haveDuplicate(array));
 
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(4);
-        arrayList.add(5);
-        arrayList.add(6);
+        printIfHaseNotDuplicate(array);
 
-//        System.out.println("Size array list - " + arrayList.size());
-//        System.out.println("Is array list empty - " + arrayList.isEmpty());
-//        System.out.println(arrayList);
+        System.out.println(countDuplicateCharInString("babayaga"));
+        System.out.println(firstNonRepeatCharInString("babayyaga"));
+        System.out.println(lengthWithoutRepeatingChar("babayyaag"));
+    }
 
-        arrayList.add(2, 3);
+    /**
+     * Is array have duplicate
+     *
+     * @param array int[] type
+     * @return boolean type
+     */
+    public static boolean haveDuplicate(int[] array) {
 
-//        System.out.println("Size array list - " + arrayList.size());
-//        System.out.println(arrayList);
+        for (int i = 0; i < array.length; i++) {
 
-        System.out.println(arrayList.size);
+            for (int j = i + 1; j < array.length; j++) {
 
-        Iterator<Integer> iterator = arrayList.iterator();
+                if (array[i] == array[j]) {
 
-        while (iterator.hasNext()) {
+                    return true;
+                }
 
-            System.out.println(iterator.next());
+            }
         }
 
+        return false;
+    }
 
-//
-//        int number = 40;
-//        System.out.println("Return index of value - " + number + " = " + arrayList.indexOf(number));
-//
-//        arrayList.set(3 ,52);
-//        System.out.println(arrayList);
-//
-//        int index = 0;
-//        System.out.println("Get the list value in index " + index + " = " + arrayList.get(index));
-//
-//        arrayList.remove(5);
-//
-//        System.out.println("Size array list - " + arrayList.size());
-//        System.out.println(arrayList);
-//
-//        int number = 4;
-//        System.out.println("Have the list value " + number + " - " + arrayList.contains(number));
-//
-//        int number1 = 10;
-//        System.out.println("Have the list value " + number1 + " - " + arrayList.contains(number1));
+    /**
+     * Print all non duplicate value
+     *
+     * @param array int[] type
+     */
+    public static void printIfHaseNotDuplicate(int[] array) {
+
+        boolean flag;
+
+        for (int i = 0; i < array.length; i++) {
+
+            flag = true;
+
+            for (int j = 0; j < array.length; j++) {
+
+                if (array[i] == array[j] && i != j) {
+
+                    flag = false;
+                }
+            }
+
+            if (flag) {
+
+                System.out.println(array[i]);
+            }
+        }
+    }
+
+    /**
+     * Count how many duplicate char in string
+     *
+     * @param str String type
+     * @return int type
+     */
+    public static int countDuplicateCharInString(String str) {
+
+        char[] ch = str.toCharArray();
+        int countChar = 0;
+
+        for (int i = 0; i < ch.length; i++) {
+
+            int count = 0;
+            for (int j = i + 1; j < ch.length; j++) {
+
+                if (ch[i] == ch[j]) {
+
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+
+                countChar++;
+            }
+        }
+
+        return countChar;
+    }
+
+    /**
+     * Returns the first non-repeated character from a given string.
+     *
+     * @param str String type
+     * @return char type
+     */
+    public static char firstNonRepeatCharInString(String str) {
+
+        char[] ch = str.toCharArray();
+        boolean flag;
+
+        for (int i = 0; i < ch.length; i++) {
+
+            flag = true;
+            for (int j = 0; j < ch.length; j++) {
+
+                if (ch[i] == ch[j] && i != j) {
+
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+
+                return ch[i];
+            }
+        }
+
+        return ' ';
+    }
+
+    /**
+     *Return length without repeating char
+     * @param str string type
+     * @return int type
+     */
+    public static int lengthWithoutRepeatingChar(String str) {
+
+        char [] ch = str.toCharArray();
+        int lengthWithoutRepeatingChar = ch.length;
+
+        for (int i = 0; i < ch.length; i++) {
+
+            for (int j = i + 1; j < ch.length; j++) {
+
+                if(ch[i] == ch[j]) {
+
+                    lengthWithoutRepeatingChar--;
+                    break;
+                }
+            }
+        }
+
+        return lengthWithoutRepeatingChar;
     }
 }
